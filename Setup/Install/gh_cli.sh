86677c7643a7f3d47c2a682393ddc361da07cb3f
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-10-13 14:56:31 -0300

    Created a main function

--- Setup/Install/gh_cli.sh
+++ Setup/Install/gh_cli.sh
@@ -17,6 +17,17 @@ function Help
    echo
 }
 
+function main
+{
+    echo "INSTALLING GITHUB CLI"
+    echo
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt install gh
+}
+
 #Take the flags
 while getopts "h" flag
 do
@@ -32,10 +43,4 @@ do
     esac
 done
 
-echo "INSTALLING GITHUB CLI"
-echo
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+main
