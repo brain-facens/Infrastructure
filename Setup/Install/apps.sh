22e027a5233c57fd0dc32f0c699a0f46775034b5
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-10-13 14:01:09 -0300

    Updated to get parameters

--- Setup/Install/apps.sh
+++ Setup/Install/apps.sh
@@ -5,18 +5,60 @@
 function Help
 {
    # Displays Help
-   echo "This script install GIT, CURL, GPARTED, HTOP, ZOOM, DISCOD, VSCODE." 
+   echo "This script install GIT, CURL, GPARTED, HTOP."
    echo
-   echo "Syntax: apps.sh [-r|h]"
+   echo "Syntax: apps.sh [-h][-r REBOOT]"
    echo "Options:"
    echo "-h     Help function"
    echo "-r     Reboot the computer after the installations"
    echo
    echo "EXAMPLE:"
-   echo "bash apps.sh -h      (Help function)"
-   echo "bash apps.sh -r true (Install the apps and reboot after)"
-   echo "bash apps.sh         (Install the apps without reboot)"
+   echo "bash apps.sh -h       (Help function)"
+   echo "bash apps.sh -r true  (Install the apps and reboot after)"
+   echo "bash apps.sh -r false (Install the apps without reboot)"
+   echo "bash apps.sh          (Install the apps without reboot)"
    echo
+}
+
+function main
+{
+    echo "UPDATING"
+    echo
+    sudo apt update
+
+    echo
+
+    echo "INSTALLING GIT"
+    echo
+    sudo add-apt-repository ppa:git-core/ppa
+    sudo apt update
+    sudo apt install git
+
+    echo
+
+    echo "INSTALLING CURL"
+    echo
+    sudo apt install curl
+
+    echo
+
+    echo "INSTALLING GPARTED"
+    echo
+    sudo apt install gparted
+
+    echo
+
+    echo "INSTALLING HTOP"
+    echo
+    sudo apt install htop
+
+    echo
+    
+    if [[ $1 = true ]]
+    then
+        echo "REBOOTING"
+        reboot
+    fi
 }
 
 #Take the flags
@@ -27,66 +69,12 @@ do
          Help
          exit
         ;;
-        r) rbt=true;;
+        r) REBOOT=$2;;
         \?)
          echo "Error: Invalid option"
          exit
         ;;
     esac
 done
-    
 
-echo "UPDATE"
-echo
-sudo apt update
-
-echo
-
-echo "INSTALLING GIT"
-echo
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt update
-sudo apt install git
-
-echo
-
-echo "INSTALLING CURL"
-echo
-sudo apt install curl
-
-echo
-
-echo "INSTALLING GPARTED"
-echo
-sudo apt install gparted
-
-echo
-
-echo "INSTALLING HTOP"
-echo
-sudo apt install htop
-
-echo
-
-echo "INSTALLING ZOOM"
-echo
-cd ~/Downloads/
-sudo apt install ./zoom_amd64.deb
-
-echo
-
-echo "INSTALLING DISCORD"
-echo
-sudo apt install ./discord-0.0.18.deb 
-
-echo
-
-echo "INSTALLING VISUAL STUDIO CODE"
-echo
-sudo apt install ./code_1.68.1-1655263094_amd64.deb
-
-if [[ $rbt = true ]]
-then
-    echo "REBOOTING"
-    reboot
-fi
+main $REBOOT
