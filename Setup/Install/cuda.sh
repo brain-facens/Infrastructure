3f3efce3ab8cb53abc950816ff34de6dd9fafc59
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-10-13 14:48:24 -0300

    Updated the CUDA install to 11.6 version

--- Setup/Install/cuda.sh
+++ Setup/Install/cuda.sh
@@ -17,6 +17,18 @@ function Help
    echo
 }
 
+function main
+{
+    echo "INSTALLING CUDA 11.6"
+    echo
+    cd ~
+    wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_510.39.01_linux.run
+    sudo sh cuda_11.6.0_510.39.01_linux.run
+    echo
+    echo "Test"
+    nvidia-smi
+}
+
 #Take the flags
 while getopts "h" flag
 do
@@ -32,15 +44,4 @@ do
     esac
 done
 
-echo "INSTALLING CUDA 11.6"
-echo
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu1804-11-6-local/7fa2af80.pub
-sudo apt-get update
-sudo apt-get -y install cuda
-echo
-echo "Test"
-nvidia-smi
+main
