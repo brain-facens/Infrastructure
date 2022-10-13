f7658160b05950f4fdad01ae3678c78c00b33c2a
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-10-13 15:36:12 -0300

    Created a main function and updated the SDK version

--- Setup/Install/sdk_zed.sh
+++ Setup/Install/sdk_zed.sh
@@ -5,16 +5,27 @@
 function Help
 {
    # Displays Help
-   echo "This script install Zed SDK." 
+   echo "This script install Zed SDK 3.7 to Ubuntu 18." 
+   echo "This SDK version is compatible with CUDA 11.X (11.0*->11.7)"
    echo
    echo "Syntax: sdk_zed.sh [-h]"
-   echo "Options:"
+   echo "Options:"  
    echo "-h     Help function"
    echo
    echo "EXAMPLE:"
    echo "bash sdk_zed.sh -h   (Help function)"
    echo "bash sdk_zed.sh      (Install Zed SDK)"
    echo
+}
+
+function main
+{
+    echo "INSTALLING ZED SDK 3.7"
+    echo
+    cd ~/Downloads
+    wget https://stereolabs.sfo2.cdn.digitaloceanspaces.com/zedsdk/3.7/ZED_SDK_Ubuntu18_cuda11.7_v3.7.7.run
+    chmod +x ZED_SDK_Ubuntu18_cuda11.7_v3.7.7.run
+    ./ZED_SDK_Ubuntu18_cuda11.7_v3.7.7.run
 }
 
 #Take the flags
@@ -32,8 +43,4 @@ do
     esac
 done
 
-echo "INSTALLING ZED SDK"
-echo
-cd ~/Downloads
-chmod +x ZED_SDK_Ubuntu18_cuda11.5_v3.7.4.run
-./ZED_SDK_Ubuntu18_cuda11.5_v3.7.4.run
+main
