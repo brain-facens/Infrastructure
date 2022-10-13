e7ba213f2c0c11569e4c9341fe488d331883dc93
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-10-13 15:10:26 -0300

    Added a main function and get the parameters

--- Setup/Install/pytorch.sh
+++ Setup/Install/pytorch.sh
@@ -7,16 +7,30 @@ function Help
    # Displays Help
    echo "This script install PyTorch to CUDA 11.6." 
    echo
-   echo "Syntax: pytorch.sh [-p|h]"
+   echo "Syntax: pytorch.sh [-h][-p VERSION]"
    echo "Options:"
    echo "-h     Help function"
-   echo "-p     Python version that will be installed"
+   echo "-p     Choose the python version that will be installed"
    echo
    echo "EXAMPLE:"
    echo "bash pytorch.sh -h      (Help function)"
    echo "bash pytorch.sh -p 3.8  (Install PyTorch in Python3.8)"
-   echo "bash pytorch.sh         (Install PyTorch in Python3)"
+   echo "bash pytorch.sh         (Install PyTorch with pip)"
    echo
+}
+
+function main
+{
+    if [[ $1 -eq 0 ]]
+    then
+        echo "INSTALLING PYTORCH TO CUDA 11.6 WITH 'pip'"
+        echo
+        pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+    else
+        echo "INSTALLING PYTORCH TO CUDA 11.6 WITH 'python$1 -m pip'"
+        echo
+        python$1 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+    fi
 }
 
 #Take the flags
@@ -27,20 +41,12 @@ do
          Help
          exit
         ;;
-        p) version=${OPTARG};;
+        p) VERSION=$2;;
         \?)
          echo "Error: Invalid option"
          exit
          ;;
     esac
 done
-if [[ $version -eq 0 ]]
-then
-    echo "INSTALLING PYTORCH TO CUDA 11.6 WITH pip3 ..."
-    echo
-    pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-else
-    echo "INSTALLING PYTORCH TO CUDA 11.6 WITH python$version -m pip ..."
-    echo
-    python$version -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-fi
+
+main $VERSION
