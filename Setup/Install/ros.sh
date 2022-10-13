8a4f388284bf46125eafa1ec6d72bfd36be86123
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-10-13 15:19:26 -0300

    Created a main function

--- Setup/Install/ros.sh
+++ Setup/Install/ros.sh
@@ -6,6 +6,7 @@ function Help
 {
    # Displays Help
    echo "This script install ROS Melodic." 
+   echo "The S.O. must be a Ubuntu 18.04."
    echo
    echo "Syntax: ros.sh [-h]"
    echo "Options:"
@@ -15,6 +16,23 @@ function Help
    echo "bash ros.sh -h   (Help function)"
    echo "bash ros.sh      (Install ROS Melodic)"
    echo
+}
+
+function main
+{
+    echo "INSTALLING ROS MELODIC"
+    echo
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt install curl
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt update
+    sudo apt install ros-melodic-desktop-full
+    echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+    source ~/.bashrc
+    sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+    sudo apt install python-rosdep
+    sudo rosdep init
+    rosdep update
 }
 
 #Take the flags
@@ -32,16 +50,4 @@ do
     esac
 done
 
-echo "INSTALLING ROS MELODIC"
-echo
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update
-sudo apt install ros-melodic-desktop-full
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install python-rosdep
-sudo rosdep init
-rosdep update
+main
